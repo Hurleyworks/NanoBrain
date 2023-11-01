f0773930162be8271f21f22f6c27c2db8eb80b68
Steve Hurley <microsoft@hurleyworks.com>
2023-11-01 19:02:02 -0400

    some cleanup

--- modules/wabi_core/excludeFromBuild/math/Maths.h
+++ modules/wabi_core/excludeFromBuild/math/Maths.h
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
 #pragma once
 
 // taken from GeometricTools Boost License https://www.geometrictools.com/
